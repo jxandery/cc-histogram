06cfdab0159c50a26a8c61aeb7effb49d55a10a8
Jack Yeh <jack.yeh@gmail.com>
2015-07-15 09:24:41 -0600

    pass test: test_it_can_convert_single_vertical_array_into_string

--- histogram_test.rb
+++ histogram_test.rb
@@ -12,4 +12,10 @@ class HistogramTest < Minitest::Test
     example = Histogram.new([3,1])
     assert_equal "xxx  x", example.stringify
   end
+
+  def test_it_can_convert_single_vertical_array_into_string
+    example = Histogram.new([3])
+    assert_equal "xxx", example.stringify
+  end
 end
+
