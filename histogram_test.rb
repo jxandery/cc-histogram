ad48836d3515c0e5a2ed0937f52d24cbfee839bf
Jack Yeh <jack.yeh@gmail.com>
2015-07-15 09:26:05 -0600

    pass test: test_it_can_convert_multiple_vertical_array_into_string

--- histogram_test.rb
+++ histogram_test.rb
@@ -17,5 +17,9 @@ class HistogramTest < Minitest::Test
     example = Histogram.new([3])
     assert_equal "xxx", example.stringify
   end
-end
 
+  def test_it_can_convert_multiple_vertical_array_into_string
+    example = Histogram.new([4,3,1,2])
+    assert_equal "xxxx xxx   x  xx", example.stringify
+  end
+end
