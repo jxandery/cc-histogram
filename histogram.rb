1f91a5db83093e39f917108fb4018d8db7d62ff9
Jack Yeh <jack.yeh@gmail.com>
2015-07-15 09:14:21 -0600

    first time for everything

---
+++ histogram.rb
@@ -0,0 +1,2 @@
+class Histogram
+end
---
+++ histogram_test.rb
@@ -0,0 +1,11 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './histogram'
+
+class HistogramTest < Minitest::Test
+
+  def test_it_exists
+   assert Histogram
+  end
+
+end
