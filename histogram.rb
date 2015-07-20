4f1e2a1ba386dc42bd9927d7411d87150a7cedc0
Jack Yeh <jack.yeh@gmail.com>
2015-07-20 17:41:58 -0600

    Application functions: - Rework structure:   + much of the structure was overly complicated    + restructured it so that methods were smaller and logic encapsulated

--- histogram.rb
+++ histogram.rb
@@ -1,9 +1,20 @@
 class Histogram
-  attr_reader :level, :input
+  attr_reader :level, :input, :counter, :arr #:index
 
   def initialize(input)
     @level = input.max
     @input = input
+    #@index = 0
+    @counter = 1
+    @arr = hash_setup
+  end
+
+  def hash_setup
+    @arr = {}
+    (1..level).to_a.map do |i|
+      arr[i] = []
+    end
+    arr
   end
 
   def stringify
@@ -16,9 +27,51 @@ class Histogram
     stringify.chars.each_slice(level).to_a.transpose
   end
 
-  def find_width
-    transpose.map do |t|
-      t.count('x')
+  def single_column?(items, index)
+    (items[index] == 'x' && items[index + 1] == ' ') || (items[index] == 'x' && items[index + 1].nil?)
+  end
+
+  def multiple_columns?(items, index)
+    items[index] == 'x' && items[index + 1] == 'x'
+  end
+
+  def widths(items)
+    col_counter = 1
+    items.each_with_index do |item, index|
+      if multiple_columns?(items, index)
+        col_counter += 1
+      elsif single_column?(items, index)
+        @arr[level] << col_counter
+      else
+        col_counter = 1
+      end
+        index += 1
+    end
+  end
+
+  def all_widths
+   transpose.each do |input|
+     widths(input)
+     @level -= 1
+   end
+   arr
+  end
+
+  def max_retangle
+    all_widths
+    arr.map do |key, value|
+      key * value.max
+    end.max
+  end
+
+  def find_all_width
+    transpose.map do |items|
+      width_counter = 0
+      items.map.each_with_index do |item, index|
+        widths(item, index)
+        width_counter += 1
+        arr
+      end
     end
   end
 end
--- histogram_test.rb
+++ histogram_test.rb
@@ -8,6 +8,11 @@ class HistogramTest < Minitest::Test
     assert Histogram
   end
 
+  def test_hash_setup
+    example = Histogram.new([3,1])
+    assert_equal [], example.arr[2]
+  end
+
   def test_it_can_convert_vertical_arrays_into_string
     example = Histogram.new([3,1])
     assert_equal "xxx  x", example.stringify
@@ -38,19 +43,58 @@ class HistogramTest < Minitest::Test
     assert_equal [['x', ' ', ' ', ' '], ['x', 'x', ' ', ' '], ['x', 'x', ' ', 'x'], ['x', 'x', 'x', 'x']], example.transpose
   end
 
-  def test_it_can_transpose_vertical_arrays
-    example = Histogram.new([3,1])
-    assert_equal [1,1,2], example.find_width
+  def test_adjacent_one_column
+    example = Histogram.new([3,1,2])
+    example.widths(['x', ' ', ' '])
+    assert_equal [1], example.arr[3]
   end
 
-  def test_it_can_transpose_multiple_vertical_array
-    example = Histogram.new([4,3,1,2])
-    assert_equal [1, 2, 3, 4], example.find_width
+  def test_adjacent_two_column
+    example = Histogram.new([3,3,1])
+    example.widths(['x', 'x', ' '])
+    assert_equal [2], example.arr[3]
   end
 
+  def test_adjacent_three_column
+    example = Histogram.new([3,3,3])
+    example.widths(['x', 'x', 'x'])
+    assert_equal [3], example.arr[3]
+  end
 
-  def test_it_can_find_all_widths_for_two_columns
-    example = Histogram.new([3,1])
-    assert_equal [[1,0], [1,0], [1,1]], example.find_all_width
+  def test_adjacent_single_column
+    example = Histogram.new([3,1,3])
+    example.widths(['x', ' ', 'x'])
+    assert_equal [1,1], example.arr[3]
+  end
+
+  def test_multiple_adjacent_single_column
+    example = Histogram.new([3,3,1,3])
+    example.widths(['x', 'x', ' ', 'x'])
+    assert_equal [2,1], example.arr[3]
+  end
+
+  def test_multiple_adjacent_multiple_column
+    example = Histogram.new([3,3,1,3,3])
+    example.widths(['x', 'x', ' ', 'x', 'x'])
+    assert_equal [2,2], example.arr[3]
+  end
+
+  def test_find_width_for_multiple_levels
+    example = Histogram.new([2,2,1,2,2])
+    input = [['x', 'x', ' ', 'x', 'x'], ['x', 'x', 'x', 'x', 'x']]
+    example.all_widths
+    assert_equal({2=>[2,2], 1=>[5]}, example.arr)
+  end
+
+  def test_find_width_for_multiple_levels2
+    example = Histogram.new([3,2,1,2,3])
+    input = [['x', ' ', ' ', ' ', 'x'],['x', 'x', ' ', 'x', 'x'], ['x', 'x', 'x', 'x', 'x']]
+    example.all_widths
+    assert_equal({3=> [1,1], 2=>[2,2], 1=>[5]}, example.arr)
+  end
+
+  def test_find_max_rectangle
+    example = Histogram.new([3,2,1,2,3])
+    assert_equal 5, example.max_retangle
   end
 end
